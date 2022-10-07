dae7f94614d1a3bdd1d2de816fb4263320e1ba9d
Dhaval Thakkar <dhavalthakkar1541@gmail.com>
2022-10-07 12:33:57 +0530

    Still trying to understand, going through themes.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Editor } from "./components/Editor";
+import EditorNpm from "./components/EditorNpm";
+import LexicalEditor from "./components/LexicalEditor";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Lexical Editor</h1>
+      {/* {Editor} */}
+      {/* <Editor /> */}
+      {/* {LexicalEditor} */}
+      {/* <LexicalEditor /> */}
+
+      <EditorNpm />
+    </>
   );
 }
 
---
+++ src/components/LexicalEditor.js
@@ -0,0 +1,94 @@
+import React from "react";
+import { $getRoot, $getSelection, $isRangeSelection } from "lexical";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { mergeRegister } from "@lexical/utils";
+import clsx from "clsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function onChange(state) {
+  state.read(() => {
+    const root = $getRoot();
+    const selection = $getSelection();
+    console.log(selection);
+  });
+}
+function LexicalEditor() {
+  return (
+    <div className="bg-white relative rounded-sm">
+      <h1>Hello</h1>
+      <LexicalComposer
+        initialConfig={{
+          theme: {
+            paragraph: "mb-1", // tailwind classes work!
+          },
+          onError(error) {
+            throw error;
+          },
+        }}
+      >
+        <RichTextPlugin
+          contentEditable={
+            <ContentEditable className="h-[450px] outline-none py-[15px] px-2.5 resize-none overflow-hidden text-ellipsis" />
+          }
+          placeholder={
+            <div className="absolute top-[15px] left-[10px] pointer-events-none select-none">
+              Now write something brilliant...
+            </div>
+          }
+        />
+        <OnChangePlugin onChange={onChange} />
+        <HistoryPlugin />
+      </LexicalComposer>
+    </div>
+  );
+}
+
+export default LexicalEditor;
+
+const Toolbar = () => {
+  const [editor] = useLexicalComposerContext();
+  const [isBold, setIsBold] = React.useState(false);
+
+  const updateToolbar = React.useCallback(() => {
+    const selection = $getSelection();
+    if ($isRangeSelection(selection)) {
+      setIsBold(selection.hasFormat("bold"));
+    }
+  }, [editor]);
+
+  React.useEffect(() => {
+    return mergeRegister(
+      editor.registerUpdateListener(({ editorState }) => {
+        editorState.read(() => {
+          updateToolbar();
+        });
+      })
+    );
+  }, [updateToolbar, editor]);
+
+  return (
+    <div className="absolute z-20 bottom-0 left-1/2 transform -translate-x-1/2 min-w-52 h-10 px-2 py-2 bg-[#1b2733] mb-4 space-x-2 flex items-center">
+      <button
+        className={clsx(
+          "px-1 hover:bg-gray-700 transition-colors duration-100 ease-in",
+          isBold ? "bg-gray-700" : "bg-transparent"
+        )}
+        onClick={() => {
+          console.log("CLICK");
+          // editor.dispatchCommand(FORMAT_TEXT_COMMAND, "bold");
+        }}
+      >
+        <FontAwesomeIcon
+          icon="fa-solid fa-bold"
+          className="text-white w-3.5 h-3.5"
+        />
+      </button>
+      {/* ... */}
+    </div>
+  );
+};
